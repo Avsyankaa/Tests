29baf72bcbe24f23f19719c184b2e8e2ee62c5c2
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-10-01 12:04:02 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -9,8 +9,20 @@ TEST(Task1, Middle) {
   float result = mean(v);
   EXPECT_EQ(result, 30);
     // отрицательные коэфиициенты 
-   std::vector<float> v = {-11, -2, -36, -47, -5, -53, -56}; 
+  std::vector<float> v = {-11, -2, -36, -47, -5, -53, -56}; 
   float result = mean(v);
   EXPECT_EQ(result, -30);
+    // положительные и отрицательные коэфиициенты
+  std::vector<float> v = {5, -3, -5, 47, -40, 3, 0}; 
+  float result = mean(v);
+  EXPECT_EQ(result, 1);  
+    // вектор из одного элемента
+  std::vector<float> v = {456}; 
+  float result = mean(v);
+  EXPECT_EQ(result, 456);    
+    // пустой вектор
+  std::vector<float> v = {}; 
+  float result = mean(v);
+  EXPECT_EQ(result, 0);     
 }
 
