4f38e21eb3bb17d3238e634af6fe23768886a954
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-10-01 11:24:35 +0300

    Update lab05.cpp

--- lab05.cpp
+++ lab05.cpp
@@ -1,5 +1,5 @@
 #include "lab05.hpp"
-float mean(const std::vector<float>& array) {
+float mean(std::vector<float>& array) {
     float sum;
     if (array.size()==0) {
         return 0;
@@ -12,7 +12,7 @@ return sum;
 };
 
 int main() {
-    const std::vector<float> v = {11, 2, 36, 47, 5, 53, 56};
+  std::vector<float> v = {11, 2, 36, 47, 5, 53, 56};
   float result = mean(v);
   std::cout<< result<< std::endl;
 }
