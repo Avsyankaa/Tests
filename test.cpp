39582900f309f7893cdda28e38234755bcae3a8b
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-10-01 11:23:19 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@
 
 TEST(Task1, Middle) {
     // базовый сценарий
-  std::vector<float> v = {11, 2, 36, 47, 5, 53, 56};
+  const std::vector<float> v = {11, 2, 36, 47, 5, 53, 56};
   float result = mean(v);
   EXPECT_EQ(result, 30);
 }
