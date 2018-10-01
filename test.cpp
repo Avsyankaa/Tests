7f6913096464ade907b389ab66648376236aa674
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-10-01 12:25:44 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -26,3 +26,25 @@ TEST(Task1, Middle) {
   EXPECT_EQ(result, 0);     
 }
 
+TEST(Task3, Argmax) {
+    // базовый сценарий
+  std::vector<float> v1 = {11, 2, 36, 47, 5, 53, 56};
+  int index_max=argmax(v1);
+  EXPECT_EQ(index_max, 6);  
+    // отрицательные коэфиициенты 
+  std::vector<float> v2 = {-11, -2, -36, -47, -5, -53, -56}; 
+  index_max = argmax(v2);
+  EXPECT_EQ(index_max, 1);
+    // положительные и отрицательные коэфиициенты
+  std::vector<float> v3 = {5, -3, -5, 47, -40, 3, 0}; 
+  index_max = argmax(v3);
+  EXPECT_EQ(index_max, 3);  
+    // вектор из одного элемента
+   std::vector<float> v4 = {456};  
+   index_max = argmax(v4);
+   EXPECT_EQ(index_max, 0);   
+    // пустой вектор
+  std::vector<float> v5 = {0};  
+  index_max = argmax(v5);
+  EXPECT_EQ(index_max, -1);   
+}
